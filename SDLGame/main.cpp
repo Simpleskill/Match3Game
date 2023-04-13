bd19263399b026f88825fcd1a935847e7fe11f55
Simpleskill <andrefcastro1@hotmail.com>
2023-04-13 05:04:03 +0100

    Static libraries and release version

--- SDLGame/main.cpp
+++ SDLGame/main.cpp
@@ -1,16 +1,16 @@
-#include "Game.hpp"
-
-int main(int argc, char* argv[])
-{
-	Game game;
-
-	game.init("JewelCrush", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
-	while (game.running()) {
-		game.handleEvents();
-		game.update();
-		game.render();
-	}
-	game.clean();
-
-	return 0;
-}
+//#include "Game.hpp"
+//
+//int main(int argc, char* argv[])
+//{
+//	Game game;
+//
+//	game.init("JewelCrush", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+//	while (game.running()) {
+//		game.handleEvents();
+//		game.update();
+//		game.render();
+//	}
+//	game.clean();
+//
+//	return 0;
+//}
